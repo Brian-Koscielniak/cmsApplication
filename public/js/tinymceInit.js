964636b8c843f8ec53c500634785c10119ff72ff
Brian Koscielniak <bkoscielniak31@gmail.com>
2015-12-03 12:39:26 -0500

    Moved tinymce init to exteral script / adjusted settings

---
+++ public/js/tinymceInit.js
@@ -0,0 +1,7 @@
+tinymce.init({
+	menubar: false,
+	selector:'#content', 
+	content_css:'../css/tinyMCE.css', 
+	plugins:'save',
+	toolbar:'styleselect | bold | italic | alignleft | aligncenter | alignright | alignjustify | bullist | numlist | outdent | indent | link image | code'
+});
